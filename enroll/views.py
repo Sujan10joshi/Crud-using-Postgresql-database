ab2de73e2f2e1e1ad62fa05744e0bda4e21c7fc0
Sujan <suzan7zoc@gmail.com>
2024-02-12 21:46:24 +0545

    Update and delete data of student

--- enroll/views.py
+++ enroll/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponseRedirect
 from .forms import StudentRegistration
 from .models import User
 
-# Create your views here.
-
+# This function will add and show data
 def add_show(request):
     if request.method == 'POST':
         fm = StudentRegistration(request.POST)
@@ -19,3 +18,22 @@ def add_show(request):
         fm = StudentRegistration()
     stud = User.objects.all()
     return render(request, 'enroll/addandshow.html', {'form':fm,'stu':stud,})
+
+# This function will update data
+def update_data(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(request.POST, instance=pi)
+        if fm.is_valid():
+            fm.save()
+    else:
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(instance=pi)
+    return render(request, 'enroll/update.html', {'form':fm})
+
+# This function will delete data
+def delete_data(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect("/")
