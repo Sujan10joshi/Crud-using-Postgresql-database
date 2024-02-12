f5646f3c4426951bdf3afa35383661fdb3c012b8
Sujan <suzan7zoc@gmail.com>
2024-02-12 18:58:48 +0545

    Adding and Showing data of students

--- enroll/views.py
+++ enroll/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import StudentRegistration
+from .models import User
 
 # Create your views here.
 
 def add_show(request):
-    return render(request, 'enroll/addandshow.html')
+    if request.method == 'POST':
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm,email=em,password=pw)
+            reg.save()
+            return HttpResponseRedirect("/")
+    else:
+        fm = StudentRegistration()
+    stud = User.objects.all()
+    return render(request, 'enroll/addandshow.html', {'form':fm,'stu':stud,})
